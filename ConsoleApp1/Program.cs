942f28e387096b908d95b2f2afd6aa0fd4d9e036
Мария <mahadrum@yandex.ru>
2024-05-31 13:30:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man a = new Man("Алан", Race.mongol);
+            Console.WriteLine(Man.count);
+
+            Man b = new Man("Чингачкуг", Race.americ);
+            Console.WriteLine(Man.count);
+
+            Man с = new Man("Ын", Race.mongol);
+            Console.WriteLine(Man.count);
+
+            Man с = new Man("Мин", Race.mongol);
+            Console.WriteLine(Man.count);
+        }
+    }
+}
---
+++ ConsoleApp1/man.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Race
+        {
+        public static string euro = "европиоидная";
+        public static string mongol = "монголоидная";
+        public static string americ = "американоидная";
+
+    }
+     
+    public class Man
+    {
+        static public int count = 0;
+
+        public string name;
+        public string race;
+
+        public Man(string name, string race)
+        {
+            this.name = name;
+            this.race = race;
+            count++;
+        }
+    }
+}
